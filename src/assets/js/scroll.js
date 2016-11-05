6269fa9277be7c88e51a0b40043574d08809e8fa
Lisi Linhart <liisilein@hotmail.com>
2016-11-05 23:56:29 +0100

    Descriptions added

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -1,47 +1,46 @@
-(function () {
+(function() {
   // Elements
   var navItems = document.getElementsByClassName('js-nav-item');
   var sections = document.getElementsByClassName('js-section');
   var subSections = [].slice.call(document.getElementsByClassName('sg-section__group'));
   var headings = [].slice.call(document.getElementsByClassName('sg-section__heading'));
 
-
   // Initial states
-  TweenLite.set(subSections, {autoAlpha: 0});
-  TweenMax.staggerFromTo([headings,subSections[0]], 0.6, 
-    {autoAlpha:0, x: 40},
-    {autoAlpha:1, x:0, ease: Power2.easeInOut, delay: 0.4}
-    , 0.1);
+  TweenLite.set(subSections, { autoAlpha: 0 });
 
   // Set Click Events on Navigation
-  for(var i = 0; i < navItems.length; i++){
-    var elem = navItems[i]; 
+  for (var i = 0; i < navItems.length; i++) {
+    var elem = navItems[i];
+
     // ScrollTo Function  
-    elem.onclick = function(e){
+    elem.onclick = function(e) {
       e.preventDefault();
       e.stopPropagation();
-      let id = String(e.target.getAttribute('href')).replace("#","");
-      TweenLite.to(window, 1.6, {scrollTo: sections[id].offsetTop - 70, ease: Power2.easeInOut} );
+      var id = String(e.target.getAttribute('href')).replace("#", "");
+      history.pushState(null, null, '#' + id);
+      TweenLite.to(window, 1.6, {
+        scrollTo: sections[id].offsetTop - 70,
+        ease: Power2.easeInOut
+      });
     };
   }
 
-  // animate in elements when scrolled in
+  // animate in elements window scrolled
   window.addEventListener('scroll', function() {
-    for(var i = 0; i < subSections.length; i++){
-      var elem = subSections[i]; 
-      if (isInViewport(elem) && elem.style.opacity == 0) {
-        console.log(elem)
-          TweenLite.fromTo(elem, 0.7, {
-            autoAlpha: 0, 
-            x: 50
-          },{
-            x: 0,
-            autoAlpha: 1, 
-            ease: Power4.easeOut
-          } );
-          subSections.splice( i, 1 );
-      }
-    }
+    checkElements(subSections);
+  });
+
+  // animates elements in when page loaded
+  document.addEventListener("DOMContentLoaded", function() {
+    checkElements(subSections);
+    TweenMax.staggerFromTo(headings, 0.6, {
+      autoAlpha: 0,
+      x: 40
+    }, {
+      autoAlpha: 1,
+      x: 0,
+      ease: Power2.easeInOut,
+    }, 0.1);
   });
 
   // Helpers
@@ -51,8 +50,25 @@
     return (
       rect.bottom >= 0 &&
       //rect.left >= 0 &&
-      rect.top <= (window.innerHeight || html.clientHeight) 
+      rect.top <= (window.innerHeight || html.clientHeight)
       //rect.right <= (window.innerWidth || html.clientWidth)
-      );
+    );
+  }
+
+  function checkElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+      var elem = elements[i];
+      if (isInViewport(elem) && elem.style.opacity == 0) {
+        TweenLite.fromTo(elem, 0.7, {
+          autoAlpha: 0,
+          x: 50
+        }, {
+          x: 0,
+          autoAlpha: 1,
+          ease: Power4.easeOut
+        });
+        elements.splice(i, 1);
+      }
+    }
   }
 }());
